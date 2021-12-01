0812024f453c54afd52c8d6c5fc31d73cd2318c1
Daryl LaBar <daryl@gapintegrity.com>
2021-12-01 17:34:50 -0500

    Pre Errors

--- DLaB.Xrm.Tests/ExtensionsIOrganizationServiceTests.cs
+++ DLaB.Xrm.Tests/ExtensionsIOrganizationServiceTests.cs
@@ -1,5 +1,8 @@
 ﻿
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using DLaB.Xrm.Entities;
 using DLaB.Xrm.Test;
 using DLaB.Xrm.Test.Core.Builders;
@@ -47,5 +50,68 @@ namespace Core.DLaB.Xrm.Tests
         }
 
         #endregion FirstOrDefault
+
+        #region CreateOrMinimalUpdate
+
+        [TestMethod]
+        public void Extensions_IOgranizationService_CreateOrMinimalUpdate()
+        {
+            new CreateOrMinimalUpdate().Test();
+        }
+
+        // ReSharper disable once InconsistentNaming
+        private class CreateOrMinimalUpdate : TestMethodClassBase
+        {
+            private struct Ids
+            {
+                public static readonly Id<Contact> Contact = new Id<Contact>("74B36BB1-92F6-4E47-801B-75120931B8C5");
+            }
+
+            protected override void Test(IOrganizationService service)
+            {
+                service = new OrganizationServiceBuilder(service).WithIdsDefaultedForCreate(Ids.Contact).Build();
+                var entity = new Contact
+                {
+                    NumberOfChildren = 3,
+                    Address1_City = "Any Town",
+                    Address1_AddressTypeCodeEnum = Contact_Address1_AddressTypeCode.Primary,
+                };
+
+                // Should Create
+                service.CreateOrMinimumUpdate(entity);
+                AssertCrm.Exists(Ids.Contact);
+
+                // Should not update or create
+                var readOnly = new OrganizationServiceBuilder(service).IsReadOnly().Build();
+                readOnly.CreateOrMinimumUpdate(entity);
+
+                // Should update only single value;
+                var entitiesById = new Dictionary<Guid, Contact>
+                {
+                    {entity.Id, entity.Clone(true)}
+                };
+                entity.Address1_Country = "USA";
+
+                var updater = new TestUpdater(entitiesById);
+                service.CreateOrMinimumUpdate(entity, updater);
+                Assert.AreEqual(1, updater.MostRecentUnchangedAttributes.Count);
+                Assert.AreEqual(Contact.Fields.Address1_Country, updater.MostRecentUnchangedAttributes.First());
+            }
+        }
+
+        public class TestUpdater : MinimumUpdaterDefault<Contact>
+        {
+            public TestUpdater(Dictionary<Guid, Contact> dict) : base(dict) { }
+
+            public List<string> MostRecentUnchangedAttributes { get; private set; }
+
+            public override void PreMinimalUpdate(Contact entity, Contact minimalChangesEntity, List<string> unchangedAttributes)
+            {
+                base.PreMinimalUpdate(entity, minimalChangesEntity, unchangedAttributes);
+                MostRecentUnchangedAttributes = unchangedAttributes;
+            }
+        }
+
+        #endregion CreateOrMinimalUpdate
     }
 }
