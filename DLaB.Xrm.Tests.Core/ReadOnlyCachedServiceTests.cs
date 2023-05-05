4e706b2ad23f6d6121015adc86c52315b435fa92
Daryl LaBar <d3labar@hotmail.com>
2023-05-04 20:51:52 -0400

    Add Default Caching Support for all retrieve type of requests #53

--- DLaB.Xrm.Tests.Core/ReadOnlyCachedServiceTests.cs
+++ DLaB.Xrm.Tests.Core/ReadOnlyCachedServiceTests.cs
@@ -1,9 +1,17 @@
 ﻿#if NET
 using DataverseUnitTest;
+using DataverseUnitTest.Builders;
+#else
+using DLaB.Xrm.Test.Builders;
 #endif
+
+using System;
 using DLaB.Xrm.Entities;
 using DLaB.Xrm.Test;
+using Microsoft.Crm.Sdk.Messages;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Xrm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Metadata;
 using Microsoft.Xrm.Sdk.Query;
 using Source.DLaB.Xrm;
 
@@ -53,6 +61,82 @@ namespace DLaB.Xrm.Tests.Core
             // Different Where
             contacts = sut.GetEntities<Contact>(new ConditionExpression(Contact.Fields.FirstName, ConditionOperator.NotNull));
             Assert.AreEqual(0, contacts.Count);
+        }
+
+        [TestMethod]
+        public void RetrieveEntityRequest_Should_CacheValue()
+        {
+            TestInitializer.InitializeTestSettings();
+            var contactCount = 0;
+            var nonContactCount = 0;
+            var service = new OrganizationServiceBuilder(TestBase.GetOrganizationService())
+                .WithFakeExecute((_, r) =>
+                {
+                    var name = ((RetrieveEntityRequest)r).LogicalName;
+                    if (name == Contact.EntityLogicalName)
+                    {
+                        contactCount++;
+                    }
+                    else
+                    {
+                        nonContactCount++;
+                    }
+
+                    var retrieveResponse = new RetrieveEntityResponse();
+                    retrieveResponse.Results.Add(nameof(RetrieveEntityResponse.EntityMetadata), new EntityMetadata
+                    {
+                        LogicalName = name,
+                    });
+                    return retrieveResponse;
+                }).Build();
+
+
+            var response = (RetrieveEntityResponse)service.Execute(new RetrieveEntityRequest{ LogicalName = Contact.EntityLogicalName });
+            Assert.AreEqual(Contact.EntityLogicalName, response.EntityMetadata.LogicalName);
+            Assert.AreEqual(1, contactCount);
+            Assert.AreEqual(0, nonContactCount);
+
+            var sut = new ReadOnlyCachedService(service);
+            response = (RetrieveEntityResponse)sut.Execute(new RetrieveEntityRequest { LogicalName = Contact.EntityLogicalName });
+            Assert.AreEqual(Contact.EntityLogicalName, response.EntityMetadata.LogicalName);
+            Assert.AreEqual(2, contactCount);
+            Assert.AreEqual(0, nonContactCount);
+
+            // Cached
+            response = (RetrieveEntityResponse)sut.Execute(new RetrieveEntityRequest { LogicalName = Contact.EntityLogicalName });
+            Assert.AreEqual(Contact.EntityLogicalName, response.EntityMetadata.LogicalName);
+            Assert.AreEqual(2, contactCount);
+            Assert.AreEqual(0, nonContactCount);
+
+            // Different Entity, new request that gets cached
+            response = (RetrieveEntityResponse)sut.Execute(new RetrieveEntityRequest { LogicalName = Account.EntityLogicalName });
+            Assert.AreEqual(Account.EntityLogicalName, response.EntityMetadata.LogicalName);
+            Assert.AreEqual(2, contactCount);
+            Assert.AreEqual(1, nonContactCount);
+
+            // Cached
+            response = (RetrieveEntityResponse)sut.Execute(new RetrieveEntityRequest { LogicalName = Account.EntityLogicalName });
+            Assert.AreEqual(Account.EntityLogicalName, response.EntityMetadata.LogicalName);
+            Assert.AreEqual(2, contactCount);
+            Assert.AreEqual(1, nonContactCount);
+        }
+
+        [TestMethod]
+        public void NonRetrieveRequest_Should_Error()
+        {
+            TestInitializer.InitializeTestSettings();
+            var service = TestBase.GetOrganizationService();
+            var sut = new ReadOnlyCachedService(service);
+
+            try
+            {
+                sut.Execute(new WhoAmIRequest());
+            }
+            catch (NotImplementedException)
+            {
+                return;
+            }
+            Assert.Fail("Should have thrown NotImplementedException!");
         }
     }
 }
