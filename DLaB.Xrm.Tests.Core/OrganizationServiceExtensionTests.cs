c42ddbd7db664962fa5a4db440ac8bdce4fb6d57
Daryl LaBar <d3labar@hotmail.com>
2023-10-21 14:59:01 -0400

    Add additional Testing

--- DLaB.Xrm.Tests.Core/OrganizationServiceExtensionTests.cs
+++ DLaB.Xrm.Tests.Core/OrganizationServiceExtensionTests.cs
@@ -1,17 +1,17 @@
 ﻿#if NET
 using DataverseUnitTest;
-using DataverseUnitTest.Builders;
 #else
-using DLaB.Xrm.Test.Builders;
 #endif
 
 using System;
 using DLaB.Xrm.Entities;
 using DLaB.Xrm.Test;
+using DLaB.Xrm.Test.Core.Builders;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Microsoft.Xrm.Sdk;
 using Microsoft.Xrm.Sdk.Query;
 using Source.DLaB.Xrm;
+using OrganizationServiceBuilder = DLaB.Xrm.Test.Core.Builders.OrganizationServiceBuilder;
 
 namespace DLaB.Xrm.Tests.Core
 {
@@ -60,7 +60,7 @@ namespace DLaB.Xrm.Tests.Core
             bu.Address2_Fax = "123-456-7890";
             service.Update(bu);
             service = new OrganizationServiceBuilder(service)
-                .WithFakeUpdateForEntity(bu.ToEntityReference(), e =>
+                .WithFakeUpdateForEntity(bu.ToEntityReference(), _ =>
                 {
                     updatedEntity = bu.ToEntity<BusinessUnit>();
                 }).Build();
@@ -69,5 +69,56 @@ namespace DLaB.Xrm.Tests.Core
 
             Assert.AreEqual(bu.Address2_Fax, updatedEntity.Address2_Fax);
         }
+
+        #region AcquireLock_Should_UpdateSpecificEmail
+
+        [TestMethod]
+        public void AcquireLock_Should_UpdateSpecificEmail()
+        {
+            new Should_UpdateSpecificEmail().Test();
+        }
+
+        // ReSharper disable once InconsistentNaming
+        private class Should_UpdateSpecificEmail : TestMethodClassBase
+        {
+            /// <summary>
+            /// The string "ABC" returned 360 as the random value with the hash code
+            /// </summary>
+            private const double ExpectedDayDiff = -360d;
+            private struct Ids
+            {
+                public struct Emails
+                {
+                    public static readonly Id<Email> A = new Id<Email>("73E70E4F-B81F-400A-A9A0-3371043E4BFC");
+                    public static readonly Id<Email> B = new Id<Email>("4EF2E668-FCE2-4ABE-B67C-A355B370678C");
+                    public static readonly Id<Email> C = new Id<Email>("CC831270-0A63-41B0-8F86-F6A055B837CD");
+                }
+            }
+
+            protected override void InitializeTestData(IOrganizationService service)
+            {
+                var oldest = DateTime.UtcNow.AddDays(ExpectedDayDiff - 1).Date;
+                Ids.Emails.A.Entity.OverriddenCreatedOn = oldest;
+                Ids.Emails.B.Entity.OverriddenCreatedOn = oldest.AddDays(1);
+                Ids.Emails.C.Entity.OverriddenCreatedOn = oldest.AddDays(2);
+                new CrmEnvironmentBuilder().WithEntities<Ids>().Create(service);
+            }
+
+            protected override void Test(IOrganizationService service)
+            {
+                Email updatedEmail = null;
+                service = new OrganizationServiceBuilder(service)
+                    .WithFakeUpdate((_, e) =>
+                    {
+                        updatedEmail = e.ToEntity<Email>();
+                    }).Build();
+                service.AcquireLock("ABC");
+
+                Assert.IsNotNull(updatedEmail);
+                Assert.AreEqual(Ids.Emails.B.Entity.Id, updatedEmail.Id);
+            }
+        }
+
+        #endregion GetLock_Should_UpdateSpecificEmail    
     }
 }
