3b93795041e73fba32fb3a5213ccd8b131963fac
Daryl LaBar <d3labar@hotmail.com>
2024-01-23 20:49:31 -0500

    Fix for Non Source Projects

--- DLaB.Xrm.Core/Plugin/IExtendedPlugin.cs
+++ DLaB.Xrm.Core/Plugin/IExtendedPlugin.cs
@@ -1,8 +1,10 @@
-using Source.DLaB.Xrm.Ioc;
-
 #if DLAB_UNROOT_NAMESPACE || DLAB_XRM
+using DLaB.Xrm.Ioc;
+
 namespace DLaB.Xrm.Plugin
 #else
+using Source.DLaB.Xrm.Ioc;
+
 namespace Source.DLaB.Xrm.Plugin
 #endif
 
