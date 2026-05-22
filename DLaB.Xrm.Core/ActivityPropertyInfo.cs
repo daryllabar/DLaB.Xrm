7e5638fe78339256a3e5727331220ca5a385f4bb
Daryl LaBar <9728423+daryllabar@users.noreply.github.com>
2026-05-22 17:21:10 -0400

    V5.1.1.8

--- DLaB.Xrm.Core/ActivityPropertyInfo.cs
+++ DLaB.Xrm.Core/ActivityPropertyInfo.cs
@@ -342,7 +342,7 @@ namespace Source.DLaB.Xrm
                     if (logicalName.Length > 4 && logicalName[3] == '_')
                     {
                         var prefix = logicalName.ToLower().Substring(0, 3);
-                        if (logicalName.ToLower().Split([prefix], StringSplitOptions.None).Length >= 3 || logicalName.ToLower().EndsWith("_association"))
+                        if (logicalName.ToLower().Split(new [] { prefix }, StringSplitOptions.None).Length >= 3 || logicalName.ToLower().EndsWith("_association"))
                         {
                             // N:N Joins or association entities do not contain active flags
                             ActiveAttribute = ActiveAttributeType.None;
@@ -431,7 +431,7 @@ namespace Source.DLaB.Xrm
             // [AttributeLogicalName("statecode")], it's not a join entity
             foreach (var property in typeof(T).GetProperties())
             {
-                if (property.Name == "StateCode")
+                if (property.Name.ToLower() == "statecode")
                 {
                     return false;
                 }
